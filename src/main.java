2d86630dfb6bb8a25e425f938a9295fe36a3c528
VndrGrhrd <vando124@gmail.com>
2022-02-27 11:55:24 -0300

    initial

---
+++ src/main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Maria");
+        list.add("Alex");
+        list.add("Bob");
+        list.add("Anna");
+
+        list.add(2, "Marco"); // ADD "Marco" na posição 2 da lista
+
+        System.out.println(list.size());  // imprime o tamanho da lista
+
+        list.remove("Anna");  // DELETA A Anna
+
+        list.remove(1); // DELETA O ITE 01
+
+        list.removeIf(x -> x.charAt(0) == 'M');  // DELETA TODOS OS ITEMS EM QUE O  1º CARACTERE É 'M'
+
+        list.indexOf("Bob"); // Encontra o "Bob"
+
+//      Filtrar uma Lista
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+
+
+        //imprimir toda a lista
+        for(String x : list){
+            System.out.println(x);
+        }
+
+
+    }
+}
